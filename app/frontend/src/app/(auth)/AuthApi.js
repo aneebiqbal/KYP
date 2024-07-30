884e5e7ad224c1456cba51a96a3499d996844272
Mazher Hussain <mazherhussain1998@gmail.com>
2024-07-30 06:28:09 -0700

    api setup

---
+++ app/frontend/src/app/(auth)/AuthApi.js
@@ -0,0 +1,55 @@
+import { AuthApiService } from './AuthApiService';
+import { NextResponse } from 'next/server';
+import {setToken, destroyToken, destroyUserInfo, setUserInfo} from '../../services/JwtService';
+
+export const AuthApi = {
+  login:async (loginData) => {
+    try {await AuthApiService.login(loginData)
+      .then((response)=>{
+        setUserInfo(response.data.userInfo);
+        setToken(response.data.token);
+        NextResponse.redirect(new URL('/'));
+      })
+    } catch (e) {
+      console.log('error', e);
+      throw e.response.data;
+    }
+  },
+  signup: async(signUpData) => {
+    try {
+      return await AuthApiService.signup(signUpData);
+    } catch (e) {
+      console.log('error', e);
+      throw e.response.data;
+    }
+  },
+  forgetPassword: async(forgetPasswordData) => {
+    try {
+      return await AuthApiService.forgetPassword(forgetPasswordData);
+    } catch (e) {
+      console.log('error', e);
+      throw e.response.data;
+    }
+  },
+  resetPassword: async(resetPasswordKey,resetPasswordData) => {
+    try {
+      return await AuthApiService.resetPassword(resetPasswordKey,resetPasswordData);
+    } catch (e) {
+      console.log('error', e);
+      throw e.response.data;
+    }
+  },
+  logout: async(logoutData) => {
+    try {
+      await AuthApiService.logout(logoutData)
+        .then(()=>{
+          destroyToken();
+          destroyUserInfo();
+          NextResponse.redirect(new URL('/login'));
+        })
+    } catch (e) {
+      console.log('error', e);
+      throw e.response.data;
+    }
+  },
+};
