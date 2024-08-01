6c824bb4cabecb1c59a5712dd7dddd5a2cb98ce0
aneebiqbal <1aneebiqbal@gmail.com>
2024-08-01 16:07:08 +0500

    student structure, my-ratings

--- app/backend/src/app/student/student.service.ts
+++ app/backend/src/app/student/student.service.ts
@@ -1,26 +1,69 @@
-import { Injectable } from '@nestjs/common';
-import { CreateStudentDto } from './dto/create-student.dto';
-import { UpdateStudentDto } from './dto/update-student.dto';
+import { Professor, Rating, Student } from '@kyp/db';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
+interface CustomRatingResponse {
+  id: number;
+  name: string;
+  image_url: string;
+  department_name: string;
+  institute_name: string;
+  ratings: {
+    student_name: string;
+    student_image_url: string;
+    take_again: number;
+    love_teaching_style: number;
+    for_credit: boolean;
+    attandance: boolean;
+    upvotes: number;
+    downvotes: number;
+    reported: number;
+    tags: [];
+  }
+}
 @Injectable()
 export class StudentService {
-  create(createStudentDto: CreateStudentDto) {
-    return 'This action adds a new student';
-  }
+  constructor(
+    @InjectRepository(Professor)
+    private readonly professorRepository: Repository<Professor>,
+    @InjectRepository(Student)
+    private readonly studentRepository: Repository<Student>,
+    @InjectRepository(Rating)
+    private readonly ratingRepository: Repository<Rating>
+  ) {}
 
-  findAll() {
-    return `This action returns all student`;
-  }
+  async myRating (
+    text: string,
+    searchBy: 'professor' | 'course',
+    studentId: number
+  ) {
+    const query = await this.ratingRepository
+    .createQueryBuilder('rating')
+    .leftJoinAndSelect('rating.student', 'student')
+    .leftJoinAndSelect('rating.professor', 'professor')
+    .leftJoinAndSelect('rating.course', 'course')
+    .where('student.id = :studentId', { studentId })
 
-  findOne(id: number) {
-    return `This action returns a #${id} student`;
-  }
+    // const query = await this.ratingRepository
+    // .createQueryBuilder('student')
+    // .leftJoinAndSelect('student.rating', 'rating')
+    // .leftJoinAndSelect('rating.professor', 'professor')
+    // .leftJoinAndSelect('rating.course', 'course')
+    // .where('student.id = :studentId', { studentId })
 
-  update(id: number, updateStudentDto: UpdateStudentDto) {
-    return `This action updates a #${id} student`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} student`;
+    if (searchBy === 'professor') {
+      query.andWhere(
+        'professor.first_name ILIKE :text OR professor.last_name ILIKE :text' ,
+        {text: text}
+      )
+    } else if (searchBy === 'course') {
+      query.andWhere('course.name ILIKE :text', {text: text})
+    }
+    const rating = await query.getMany()
+    if(!rating && rating.length > 0) {
+      throw new NotFoundException('ratings not found')
+    }
+    return rating;
   }
 }
