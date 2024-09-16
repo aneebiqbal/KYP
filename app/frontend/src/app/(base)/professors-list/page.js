e2231e132c426475d06d7081294af61332d58dff
Mazher Hussain <mazherhussain1998@gmail.com>
2024-09-16 03:33:46 -0700

    react rating fixes and integration

--- app/frontend/src/app/(base)/professors-list/page.js
+++ app/frontend/src/app/(base)/professors-list/page.js
@@ -1,10 +1,14 @@
 import ProfessorsListFilter from '../../../components/ProfessorsListFilter';
+import {Suspense} from 'react';
+
 export default function page(){
 
   return<>
     <section>
       <div className="px-120 py-40 tablet-px-90 tablet-px-50 mobile-px-20">
-        <ProfessorsListFilter />
+        <Suspense>
+          <ProfessorsListFilter />
+        </Suspense>
       </div>
     </section>
   </>
